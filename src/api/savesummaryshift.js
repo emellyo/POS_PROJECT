a47551f84169710b7d97b8993e518e4f16d7053a
Bayu Emellyo <bayuemellyo@gmail.com>
2024-06-12 14:15:16 +0700

    update API

---
+++ src/api/savesummaryshift.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import ApiManager from './ApiManager';
+import RNFetchBlob from 'rn-fetch-blob';
+
+export const getsummaryshift = async data => {
+  try {
+    const urlapi = 'Service/SaveSummaryShift';
+    console.log('data: ', data);
+    console.info(ApiManager.defaults.baseURL.concat('/', urlapi));
+
+    result = await ApiManager(urlapi, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+        ArthaKey: 'jwhoC5vfL/Z01AszC3TaIQ==',
+      },
+      data: data,
+    });
+
+    // result = RNFetchBlob.config({
+    //   trusty: true,
+    // }).fetch(
+    //   'POST',
+    //   ApiManager.defaults.baseURL.concat('/', urlapi),
+    //   {
+    //     Accept: 'application/json-patch+json',
+    //     Authorization: 'jwhoC5vfL/Z01AszC3TaIQ==',
+    //     'Content-Type': 'application/json; charset=utf-8',
+    //   },
+    //   JSON.stringify(data),
+    // );
+
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
